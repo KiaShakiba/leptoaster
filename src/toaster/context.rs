61124e1c6c97c35b0778b2a3d55eaf62cc8426c9
timtom2016 <dev@timtom2016.com>
2024-12-01 18:31:36 +0100

    Minor Format changes

--- src/toaster/context.rs
+++ src/toaster/context.rs
@@ -5,19 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use std::{
-	rc::Rc,
-	cell::RefCell,
-};
+use std::{cell::RefCell, rc::Rc};
 
 use leptos::*;
 
-use crate::toast::{
-	ToastBuilder,
-	ToastData,
-	ToastId,
-	ToastLevel,
-};
+use crate::toast::{ToastBuilder, ToastData, ToastId, ToastLevel};
 
 /// The global context of the toaster. You should provide this as a global context
 /// in your root component to allow any component in your application to toast
@@ -32,159 +24,149 @@ use crate::toast::{
 ///  ```
 #[derive(Clone, Debug)]
 pub struct ToasterContext {
-	stats: Rc<RefCell<ToasterStats>>,
-	pub queue: RwSignal<Vec<ToastData>>,
+    stats: Rc<RefCell<ToasterStats>>,
+    pub queue: RwSignal<Vec<ToastData>>,
 }
 
 #[derive(Clone, Default, Debug)]
 struct ToasterStats {
-	visible: u32,
-	total: u64,
+    visible: u32,
+    total: u64,
 }
 
 impl ToasterContext {
-	/// Adds the supplied toast to the toast queue, displaying it onto the screen.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///
-	///     toaster.toast(
-	///         ToastBuilder::new("My toast message.")
-	///             .with_expiry(1_500)
-	///     );
-	/// }
-	/// ```
-	pub fn toast(&self, builder: ToastBuilder) {
-		let toast = builder.build(self.stats.borrow().total + 1);
+    /// Adds the supplied toast to the toast queue, displaying it onto the screen.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///
+    ///     toaster.toast(
+    ///         ToastBuilder::new("My toast message.")
+    ///             .with_expiry(1_500)
+    ///     );
+    /// }
+    /// ```
+    pub fn toast(&self, builder: ToastBuilder) {
+        let toast = builder.build(self.stats.borrow().total + 1);
 
-		let mut queue = self.queue.get_untracked();
-		queue.push(toast);
-		self.queue.set(queue);
+        let mut queue = self.queue.get_untracked();
+        queue.push(toast);
+        self.queue.set(queue);
 
-		self.stats.borrow_mut().visible += 1;
-		self.stats.borrow_mut().total += 1;
-	}
+        self.stats.borrow_mut().visible += 1;
+        self.stats.borrow_mut().total += 1;
+    }
 
-	/// Quickly display an `info` toast with default parameters. For more customization,
-	/// use the `toast` function.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///     toaster.info("My toast message.");
-	/// }
-	/// ```
-	pub fn info(&self, message: &str) {
-		self.toast(
-			ToastBuilder::new(message)
-				.with_level(ToastLevel::Info)
-		);
-	}
+    /// Quickly display an `info` toast with default parameters. For more customization,
+    /// use the `toast` function.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///     toaster.info("My toast message.");
+    /// }
+    /// ```
+    pub fn info(&self, message: &str) {
+        self.toast(ToastBuilder::new(message).with_level(ToastLevel::Info));
+    }
 
-	/// Quickly display a `success` toast with default parameters. For more customization,
-	/// use the `toast` function.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///     toaster.success("My toast message.");
-	/// }
-	/// ```
-	pub fn success(&self, message: &str) {
-		self.toast(
-			ToastBuilder::new(message)
-				.with_level(ToastLevel::Success)
-		);
-	}
+    /// Quickly display a `success` toast with default parameters. For more customization,
+    /// use the `toast` function.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///     toaster.success("My toast message.");
+    /// }
+    /// ```
+    pub fn success(&self, message: &str) {
+        self.toast(ToastBuilder::new(message).with_level(ToastLevel::Success));
+    }
 
-	/// Quickly display a `warn` toast with default parameters. For more customization,
-	/// use the `toast` function.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///     toaster.warn("My toast message.");
-	/// }
-	/// ```
-	pub fn warn(&self, message: &str) {
-		self.toast(
-			ToastBuilder::new(message)
-				.with_level(ToastLevel::Warn)
-		);
-	}
+    /// Quickly display a `warn` toast with default parameters. For more customization,
+    /// use the `toast` function.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///     toaster.warn("My toast message.");
+    /// }
+    /// ```
+    pub fn warn(&self, message: &str) {
+        self.toast(ToastBuilder::new(message).with_level(ToastLevel::Warn));
+    }
 
-	/// Quickly display an `error` toast with default parameters. For more customization,
-	/// use the `toast` function.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///     toaster.error("My toast message.");
-	/// }
-	/// ```
-	pub fn error(&self, message: &str) {
-		self.toast(
-			ToastBuilder::new(message)
-				.with_level(ToastLevel::Error)
-		);
-	}
+    /// Quickly display an `error` toast with default parameters. For more customization,
+    /// use the `toast` function.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///     toaster.error("My toast message.");
+    /// }
+    /// ```
+    pub fn error(&self, message: &str) {
+        self.toast(ToastBuilder::new(message).with_level(ToastLevel::Error));
+    }
 
-	/// Clears all currently visible toasts.
-	///
-	/// # Examples
-	/// ```
-	/// #[component]
-	/// fn Component() -> impl IntoView {
-	///     let toaster = expect_context::<ToasterContext>();
-	///
-	///     toaster.toast(
-	///         ToastBuilder::new("My toast message.")
-	///             .with_expiry(None) // the toast will not self-expire
-	///     );
-	///
-	///     toaster.clear();
-	/// }
-	/// ```
-	pub fn clear(&self) {
-		for toast in &self.queue.get_untracked() {
-			toast.clear_signal.set(true);
-		}
-	}
+    /// Clears all currently visible toasts.
+    ///
+    /// # Examples
+    /// ```
+    /// #[component]
+    /// fn Component() -> impl IntoView {
+    ///     let toaster = expect_context::<ToasterContext>();
+    ///
+    ///     toaster.toast(
+    ///         ToastBuilder::new("My toast message.")
+    ///             .with_expiry(None) // the toast will not self-expire
+    ///     );
+    ///
+    ///     toaster.clear();
+    /// }
+    /// ```
+    pub fn clear(&self) {
+        for toast in &self.queue.get_untracked() {
+            toast.clear_signal.set(true);
+        }
+    }
 
-	/// Removes the toast corresponding with the supplied `ToastId`.
-	pub fn remove(&self, toast_id: ToastId) {
-		let index = self.queue
-			.get_untracked()
-			.iter().enumerate()
-			.find(|(_, toast)| toast.id == toast_id)
-			.map(|(index, _)| index);
+    /// Removes the toast corresponding with the supplied `ToastId`.
+    pub fn remove(&self, toast_id: ToastId) {
+        let index = self
+            .queue
+            .get_untracked()
+            .iter()
+            .enumerate()
+            .find(|(_, toast)| toast.id == toast_id)
+            .map(|(index, _)| index);
 
-		if let Some(index) = index {
-			let mut queue = self.queue.get_untracked();
-			queue.remove(index);
-			self.queue.set(queue);
+        if let Some(index) = index {
+            let mut queue = self.queue.get_untracked();
+            queue.remove(index);
+            self.queue.set(queue);
 
-			self.stats.borrow_mut().visible -= 1;
-		}
-	}
+            self.stats.borrow_mut().visible -= 1;
+        }
+    }
 }
 
 impl Default for ToasterContext {
-	fn default() -> Self {
-		ToasterContext {
-			stats: Rc::new(RefCell::new(ToasterStats::default())),
-			queue: create_rw_signal(Vec::new()),
-		}
-	}
+    fn default() -> Self {
+        ToasterContext {
+            stats: Rc::new(RefCell::new(ToasterStats::default())),
+            queue: create_rw_signal(Vec::new()),
+        }
+    }
 }
